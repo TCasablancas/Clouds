f606e9f8d2ed7495746b3eeec8ab9b34e12960de
Thiago Silva <thyago_casablancas@hotmail.com>
2020-01-14 16:39:45 -0300

    Initial commit

---
+++ Clouds/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Clouds
+//
+//  Created by Thyago on 14/01/20.
+//  Copyright © 2020 tcasablancas. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
